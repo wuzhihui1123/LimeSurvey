4ab3dc6dbbe53ea4ed03889d1bf36d34b79d95c7
Jessica Höck <jessica.hoeck@limesurvey.org>
2019-07-19 12:37:32 +0200

    Update view.php
    Deleted TopBar Container.

--- application/views/admin/survey/Question2/view.php
+++ application/views/admin/survey/Question2/view.php
@@ -77,7 +77,7 @@
     <div class="container-fluid">
         <?php echo CHtml::form(array("admin/questionedit/update"), 'post', array('class'=>'form30 ','id'=>'frmeditquestion','name'=>'frmeditquestion')); ?>
         <input type="submit" class="hidden" name="triggerSubmitQuestionEditor" id="triggerSubmitQuestionEditor" />
-        <?=$topbarcontainer?>
+
         <div id="advancedQuestionEditor"><app /></div>
         </form>
     </div>
