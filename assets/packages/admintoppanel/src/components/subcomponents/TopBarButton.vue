105043119c6835a72aaa00a372aba8d571175503
Jessica Höck <jessica.hoeck@limesurvey.org>
2019-08-01 14:40:47 +0200

    WIP

--- assets/packages/admintoppanel/src/components/subcomponents/TopBarButton.vue
+++ assets/packages/admintoppanel/src/components/subcomponents/TopBarButton.vue
@@ -1,47 +1,46 @@
 <template>
     <div class="topbarbutton">
         <!-- Button without image and btn-success -->
-        <button v-if="button.icon === undefined && button.class === 'btn-success'"
+        <button v-if="button.icon  === undefined &&
+                      button.class === 'btn-success' &&
+                      button.id"
                 type="button"
-                :class="'btn btn-default navbar-btn white ' + button.class"
-                :href="button.href"
+                :class="'btn btn-default navbar-btn button white ' + button.class"
+                :href="button.url"
                 :id="button.id">
-                {{ button.name }}
+                1 {{ button.name }}
+       </button>
+       <!-- Button with image and btn-danger -->
+       <button v-else-if="button.class === 'btn-danger' && button.icon !== undefined"
+               type="button"
+               :class="'btn btn-default navbar-btn button white ' + button.class"
+               :href="button.url"
+               :id="button.id">
+               <span :class="'icon ' + button.icon" />
+               2 {{ button.name }}
+       </button>
+       <!-- Button with outlined image, so font will be white -->
+       <button v-else-if="button.icon.includes('-o')"
+                   type="button"
+                   :class="'btn btn-default navbar-btn button white ' + button.class"
+                   :href="button.url"
+                   :id="button.id">
+             <span :class="button.icon + ' icon'" />
+             3 {{ button.name }}
        </button>
 
-      <!-- Button with image and btn-danger -->
-      <button v-if="button.class === 'btn-danger' && button.icon !== undefined"
-              type="button"
-              :class="'btn btn-default navbar-btn white ' + button.class"
-              :href="button.url"
-              :id="button.id">
-              <span :class="'icon ' + button.icon" />
-                {{ button.name }}
-      </button>
+       <!-- Default Button -->
+       <button v-else-if="button.class &&
+                          button.icon !== undefined   ||
+                          button.icon !== 'undefined' ||
+                          button.icon !== ''"
+               type = "button"
+               :class = "'btn btn-default navbar-btn button ' + button.class"
+               :href = "button.url">
+               <span :class="button.icon + ' icon'" />
+               4 {{ button.name }}
+      </button> 
 
-      <button v-if="button.class && button.icon.includes('-o')"
-                  type="button"
-                  :class="'btn btn-default navbar-btn white ' + button.class"
-                  :href="button.url"
-                  :id="button.id">
-            <span :class="button.icon + ' icon'" />
-            {{ button.name }}
-      </button>
-      <button v-else-if="button.class"
-                  type="button"
-                  :class="'btn btn-default navbar-btn ' + button.class"
-                  :href="button.url"
-                  :id="button.id"
-                  :target="button.target">
-                  <span :class="button.icon + ' icon'" />
-                  {{ button.name }}
-      </button>
-      <!-- Default Button with Image -->
-      <button v-else-if="button.url !== '' && button.class !== '' && button.icon !== undefined"
-              type ="button" class="btn btn-default navbar-btn button" :href="button.url">
-              <span :class="button.icon + ' icon'" />
-              {{ button.name }}
-      </button>
     </div>
 </template>
 
